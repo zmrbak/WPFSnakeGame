f97dd3c52e43a152facc7a4839994b31f061c744
zmrbak <20995658@qq.com>
2019-08-22 16:32:50 +0800

    上传源码

---
+++ 配套代码/SNAKE02/SnakeGame/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeGame
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //方格大小
+        const int SnakeSquareSize = 20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawGameArea();
+        }
+
+        private void DrawGameArea()
+        {
+            int nextX = 0;
+            int nextY = 0;
+
+            Boolean netxtIsOdd = false;
+
+            while (true)
+            {
+                Rectangle rectangle = new Rectangle
+                {
+                    Width = SnakeSquareSize,
+                    Height = SnakeSquareSize,
+                    Fill = netxtIsOdd ? Brushes.White : Brushes.LightGray
+                };
+
+                GameArea.Children.Add(rectangle);
+                Canvas.SetTop(rectangle, nextY);
+                Canvas.SetLeft(rectangle, nextX);
+
+                netxtIsOdd = !netxtIsOdd;
+                nextX += SnakeSquareSize;
+
+                if(nextX>= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += SnakeSquareSize;
+                    netxtIsOdd = !netxtIsOdd;
+                }
+
+                if(nextY>=GameArea.ActualHeight)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
